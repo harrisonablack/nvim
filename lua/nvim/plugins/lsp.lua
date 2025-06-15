13b1921a25692511bfb59f6da39881f428fc291a
Harrison Black <harrison.black18@Gmail.com>
2025-06-15 13:59:13 +1000

    Update plugin versions and lock

--- lua/nvim/plugins/lsp.lua
+++ lua/nvim/plugins/lsp.lua
@@ -68,6 +68,7 @@ return {
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
 			require("mason-lspconfig").setup({
+				ensure_installed = { "jdtls" },
 				handlers = {
 					function(server_name)
 						local server = servers[server_name] or {}
