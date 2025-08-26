295d8ce46ee3058cdf258919ecde91e10d4d3a40
Harrison Black <harrison.black18@Gmail.com>
2025-08-27 00:51:16 +1000

    Tabs to 2 spaces

--- lua/nvim/settings.lua
+++ lua/nvim/settings.lua
@@ -5,10 +5,12 @@ vim.o.winborder = "rounded"
 vim.o.smartindent = true
 vim.o.termguicolors = true
 vim.o.signcolumn = "yes"
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
 vim.o.clipboard = "unnamedplus"
 vim.o.laststatus = 0
+
+vim.o.softtabstop = 2
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 
 vim.o.cmdheight = 0
 vim.g.have_nerd_font = true
