987f5a15292857980a61a2cd7be301e183594d93
Harrison Black <harrison.black18@Gmail.com>
2025-08-27 00:51:08 +1000

    Added new plugins

--- lua/nvim/plugins.lua
+++ lua/nvim/plugins.lua
@@ -1,16 +1,18 @@
 vim.pack.add({
-				{ src = "https://github.com/stevearc/oil.nvim" },
-				{ src = "https://github.com/EdenEast/nightfox.nvim" },
-				{ src = "https://github.com/echasnovski/mini.pick" },
-				{ src = "https://github.com/nvim-lualine/lualine.nvim" },
-				{ src = "https://github.com/romgrk/barbar.nvim" },
-				{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-				{ src = "https://github.com/neovim/nvim-lspconfig" },
-				{ src = "https://github.com/mason-org/mason.nvim" },
-				{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
-				{ src = "https://github.com/echasnovski/mini.completion" },
-				{ src = "https://github.com/echasnovski/mini.icons" },
-
+	{ src = "https://github.com/stevearc/oil.nvim" },
+	{ src = "https://github.com/EdenEast/nightfox.nvim" },
+	{ src = "https://github.com/echasnovski/mini.pick" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
+	{ src = "https://github.com/romgrk/barbar.nvim" },
+	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/echasnovski/mini.completion" },
+	{ src = "https://github.com/hrsh7th/nvim-cmp" },
+	{ src = "https://github.com/echasnovski/mini.icons" },
+	{ src = "https://github.com/folke/trouble.nvim" },
+	{ src = "https://github.com/windwp/nvim-autopairs" },
 })
 
 
@@ -20,6 +22,9 @@ require('barbar').setup()
 require('mini.pick').setup()
 require('mason').setup()
 require('mason-lspconfig').setup()
+require('mini.icons').setup()
+require('trouble').setup()
+require('nvim-autopairs').setup()
+
 
 require('mini.completion').setup()
-require('mini.icons').setup()
