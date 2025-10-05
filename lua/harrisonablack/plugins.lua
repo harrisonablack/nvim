eaf5dcd4a364508813314ef58db22a0d9f84267c
Harrison Black <harrison.black18@Gmail.com>
2025-10-05 17:31:45 +1100

    Removed unused plugin

--- lua/harrisonablack/plugins.lua
+++ lua/harrisonablack/plugins.lua
@@ -16,7 +16,6 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-mini/mini.icons" },
   { src = "https://github.com/nvim-mini/mini.pick" },
 	{ src = "https://github.com/nvim-mini/mini.pairs" },
-	{ src = "https://github.com/nvim-mini/mini.statusline" },
 	{ src = "https://github.com/kdheepak/lazygit.nvim.git" },
 	{ src = "https://github.com/nvim-mini/mini.bufremove.git" },
 })
