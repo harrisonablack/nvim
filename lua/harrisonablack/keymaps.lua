c3b408586f119e3b98369abc6fd692a232243a89
Harrison Black <harrison.black18@Gmail.com>
2025-10-14 19:25:45 +1100

    Organised keymaps
    added <leader>v for vsplit

--- lua/harrisonablack/keymaps.lua
+++ lua/harrisonablack/keymaps.lua
@@ -2,39 +2,31 @@ vim.g.mapleader = " "
 
 local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
-map('n', '<leader>o', ':update<CR> :source<CR>')
-map('n', '<leader>w', ':w<CR>')
-map('n', '<leader>q', ':q<CR>')
-map('n', '<leader>Q', ':wqa<CR>')
-map('n', '<C-F>', '<CMD>Open .<CR>')
-map('n', '<leader>s', ':e #<CR>')
 
--- Language format
-map('n', '<leader>lf', vim.lsp.buf.format)
+map('n', '<leader>o', ':update<CR>:source<CR>', opts)
+map('n', '<leader>w', ':w<CR>', opts)
+map('n', '<leader>q', ':q<CR>', opts)
+map('n', '<leader>Q', ':wqa<CR>', opts)
+map('n', '<leader>s', ':e #<CR>', opts)
+map('n', '<leader>bd', require("mini.bufremove").delete, opts)
+map('n', '<leader>bp', ':bp<CR>', opts)
+map('n', '<leader>v', ':vsplit<CR>', opts)
 
--- Map ; to : to avoid shift-; constantly
-map({'n', 'v', 'x'}, ';', ':')
-map({'n', 'v', 'x'}, ':', ';')
+map('n', '<C-F>', '<Cmd>Open .<CR>', opts)
+map('n', '<leader>e', '<Cmd>Oil<CR>', opts)
+map('n', '<leader>E', require("oil").open_float, opts)
+map('n', '<leader>lg', '<Cmd>LazyGit<CR>', opts)
 
--- Mini pick Buffers, Files and grep
-map('n', '<leader>g', '<Cmd>Pick grep_live<CR>')
-map('n', '<leader>r', '<Cmd>Pick buffers<CR>')
-map('n', '<leader>f', '<Cmd>Pick files<CR>')
+map('n', '<leader>lf', vim.lsp.buf.format, opts)
+map('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+map('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
 
--- Oil files
-map('n', '<leader>e', '<Cmd>Oil<CR>')
-map('n', '<leader>E', require("oil").open_float)
+map('n', '<leader>f', '<Cmd>Pick files<CR>', opts)
+map('n', '<leader>r', '<Cmd>Pick buffers<CR>', opts)
+map('n', '<leader>g', '<Cmd>Pick grep_live<CR>', opts)
 
--- Open lazygit in nvim
-map('n', '<leader>lg', '<Cmd>LazyGit<CR>')
+map('n', '<leader>xx', '<Cmd>Trouble diagnostics toggle<CR>', opts)
+map('n', '<leader>xX', '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>', opts)
 
--- Buff delete / buff previous
-map("n", "<leader>bd", require("mini.bufremove").delete, opts)
-map("n", "<leader>bp", ':bp<CR>')
-
--- Diagnostics
-map("n", "<leader>xX", "<Cmd>Trouble diagnostics toggle filter.buf=0<cr>")
-map("n", "<leader>xx", "<Cmd>Trouble diagnostics toggle<cr>")
-
-map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
-map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+map({'n', 'v', 'x'}, ';', ':', opts)
+map({'n', 'v', 'x'}, ':', ';', opts)
