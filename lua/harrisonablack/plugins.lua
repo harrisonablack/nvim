14ae134fd1758e7bc158a61d1e0313f142e62880
harrisonablack <62425169+harrisonablack@users.noreply.github.com>
2025-11-04 15:02:59 +1100

    Updated telescope styling

--- lua/harrisonablack/plugins.lua
+++ lua/harrisonablack/plugins.lua
@@ -68,3 +68,29 @@ require("blink.cmp").setup({
 		},
 	},
 })
+
+local telescope = require("telescope")
+telescope.setup({
+	defaults = {
+		preview = { treesitter = false },
+		color_devicons = true,
+		sorting_strategy = "ascending",
+		borderchars = {
+			"─", -- top
+			"│", -- right
+			"─", -- bottom
+			"│", -- left
+			"┌", -- top-left
+			"┐", -- top-right
+			"┘", -- bottom-right
+			"└", -- bottom-left
+		},
+		path_displays = { "smart" },
+		layout_config = {
+			height = 100,
+			width = 400,
+			prompt_position = "top",
+			preview_cutoff = 40,
+		}
+	}
+})
