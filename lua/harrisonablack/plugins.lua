2e1d5b749b17d0eb622b06e82a14de8691a70ccb
Harrison Black <dev+github@harrison.black>
2025-11-07 13:54:08 +1100

    Added vim-tpipeline
    Integrats Tmux and Nvim statusline

--- lua/harrisonablack/plugins.lua
+++ lua/harrisonablack/plugins.lua
@@ -20,6 +20,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-telescope/telescope-ui-select.nvim" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons.git" },
 	{ src = "https://github.com/nvim-lua/plenary.nvim.git" },
+	{ src = "https://github.com/vimpostor/vim-tpipeline.git" },
 })
 
 require("actions-preview").setup {
