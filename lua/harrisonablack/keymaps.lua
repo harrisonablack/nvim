372d69dfa04712ffe79738e5f79840a8f9e36a20
Harrison Black <dev+github@harrison.black>
2026-09-15 00:38:53 +1000

    fix(keymaps): Update keymaps, add descriptions

--- lua/harrisonablack/keymaps.lua
+++ lua/harrisonablack/keymaps.lua
@@ -1,6 +1,10 @@
 local opts = { noremap = true, silent = true }
 local map = vim.keymap.set
 
+local function with_desc(desc)
+  return vim.tbl_extend("force", opts, { desc = desc })
+end
+
 map("n", "<leader>o", ":update<CR>:source<CR>", opts)
 map("n", "<leader>w", ":w<CR>", opts)
 map("n", "<leader>q", ":q<CR>", opts)
@@ -21,43 +25,34 @@ map("n", "<C-j>", ":wincmd j<CR>", opts)
 map("n", "<C-k>", ":wincmd k<CR>", opts)
 map("n", "<C-l>", ":wincmd l<CR>", opts)
 
-map("n", "<leader>h", ":vert res -5<CR>", opts)
-map("n", "<leader>j", ":res -5<CR>", opts)
-map("n", "<leader>k", ":res +5<CR>", opts)
-map("n", "<leader>l", ":vert res +5<CR>", opts)
+map("n", "<C-S-h>", ":vert res -5<CR>", opts)
+map("n", "<C-S-j>", ":res -5<CR>", opts)
+map("n", "<C-S-k>", ":res +5<CR>", opts)
+map("n", "<C-S-l>", ":vert res +5<CR>", opts)
 
 map({ "n", "v", "x" }, ";", ":", opts)
 map({ "n", "v", "x" }, ":", ";", opts)
 
 map("n", "<Esc>", "<cmd>noh<CR>", opts)
 
-map("n", "<leader>la", vim.lsp.buf.code_action, opts)
-map("n", "<leader>lf", vim.lsp.buf.format, opts)
-map("n", "<leader>ld", vim.diagnostic.open_float, opts)
-map("n", "<leader>lt", vim.diagnostic.setloclist, opts)
-map("n", "<leader>li", vim.lsp.buf.implementation, opts)
-map("n", "<leader>lr", vim.lsp.buf.references, opts)
+map("n", "<leader>ld", vim.diagnostic.open_float, with_desc("Diagnostics: Show details"))
+map("n", "<leader>lt", vim.diagnostic.setloclist, with_desc("Diagnostics: List buffer diagnostics"))
 
-map("n", "gd", vim.lsp.buf.definition, opts)
-map("n", "gD", vim.lsp.buf.declaration, opts)
+require("which-key").add({
+  { "<leader>l", group = "LSP / Diagnostics", mode = { "n", "x" } },
+})
 
-local harpoon = require("harpoon")
+local telescope = require("telescope.builtin")
 
-map("n", "<leader>a", function()
-  harpoon:list():add()
-end, opts)
-map("n", "<leader>A", function()
-  harpoon.ui:toggle_quick_menu(harpoon:list())
-end, opts)
-map("n", "<leader>1", function()
-  harpoon:list():select(1)
-end, opts)
-map("n", "<leader>2", function()
-  harpoon:list():select(2)
-end, opts)
-map("n", "<leader>3", function()
-  harpoon:list():select(3)
-end, opts)
-map("n", "<leader>4", function()
-  harpoon:list():select(4)
-end, opts)
+map({ "n", "x" }, "<leader>la", vim.lsp.buf.code_action, with_desc("LSP: Code actions"))
+map("n", "<leader>lf", vim.lsp.buf.format, with_desc("LSP: Format buffer"))
+map("n", "<leader>ln", vim.lsp.buf.rename, with_desc("LSP: Rename symbol"))
+map("n", "<leader>ly", vim.lsp.buf.type_definition, with_desc("LSP: Go to type definition"))
+map("n", "<leader>lh", vim.lsp.buf.signature_help, with_desc("LSP: Signature help"))
+map("n", "<leader>ls", telescope.lsp_document_symbols, with_desc("LSP: Find document symbols"))
+map("n", "<leader>lw", telescope.lsp_dynamic_workspace_symbols, with_desc("LSP: Find workspace symbols"))
+map("n", "<leader>li", telescope.lsp_implementations, with_desc("LSP: Find implementations"))
+map("n", "<leader>lr", telescope.lsp_references, with_desc("LSP: Find references"))
+map("n", "gd", vim.lsp.buf.definition, with_desc("LSP: Go to definition"))
+map("n", "gD", vim.lsp.buf.declaration, with_desc("LSP: Go to declaration"))
+
