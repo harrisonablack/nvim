7cf7d4001d2171f30b6c813ebfbd3c067284c22f
Harrison Black <dev+github@harrison.black>
2026-09-17 11:43:03 +1000

    fix: update keymaps

--- lua/harrisonablack/keymaps.lua
+++ lua/harrisonablack/keymaps.lua
@@ -35,24 +35,23 @@ map({ "n", "v", "x" }, ":", ";", opts)
 
 map("n", "<Esc>", "<cmd>noh<CR>", opts)
 
-map("n", "<leader>ld", vim.diagnostic.open_float, with_desc("Diagnostics: Show details"))
-map("n", "<leader>lt", vim.diagnostic.setloclist, with_desc("Diagnostics: List buffer diagnostics"))
-
 require("which-key").add({
-  { "<leader>l", group = "LSP / Diagnostics", mode = { "n", "x" } },
+	{ "<leader>l", group = "LSP / Diagnostics", mode = { "n", "x" } },
 })
 
 local telescope = require("telescope.builtin")
 
+map("n", "<leader>lD", "<Cmd>Trouble diagnostics toggle<cr>", with_desc("LSP: Trouble global diagnostics"))
+map("n", "<leader>ld", "<Cmd>Trouble diagnostics toggle filter.buf=0<cr>", with_desc("LSP: Trouble diagnostics"))
+map("n", "<leader>lq", "<cmd>Trouble qflist toggle<cr>", with_desc("LSP: Trouble quickfix list"))
+map("n", "<leader>ll", "<cmd>Trouble loclist toggle<cr>", with_desc("LSP: Trouble location list"))
 map({ "n", "x" }, "<leader>la", vim.lsp.buf.code_action, with_desc("LSP: Code actions"))
 map("n", "<leader>lf", vim.lsp.buf.format, with_desc("LSP: Format buffer"))
-map("n", "<leader>ln", vim.lsp.buf.rename, with_desc("LSP: Rename symbol"))
+-- map("n", "<leader>ln", vim.lsp.buf.rename, with_desc("LSP: Rename symbol"))
 map("n", "<leader>ly", vim.lsp.buf.type_definition, with_desc("LSP: Go to type definition"))
-map("n", "<leader>lh", vim.lsp.buf.signature_help, with_desc("LSP: Signature help"))
+-- map("n", "<leader>lh", vim.lsp.buf.signature_help, with_desc("LSP: Signature help"))
 map("n", "<leader>ls", telescope.lsp_document_symbols, with_desc("LSP: Find document symbols"))
-map("n", "<leader>lw", telescope.lsp_dynamic_workspace_symbols, with_desc("LSP: Find workspace symbols"))
 map("n", "<leader>li", telescope.lsp_implementations, with_desc("LSP: Find implementations"))
 map("n", "<leader>lr", telescope.lsp_references, with_desc("LSP: Find references"))
 map("n", "gd", vim.lsp.buf.definition, with_desc("LSP: Go to definition"))
 map("n", "gD", vim.lsp.buf.declaration, with_desc("LSP: Go to declaration"))
-
